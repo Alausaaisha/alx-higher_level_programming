97c4702a2a51c9678790d61127de9abe9e660c94
Alausa Aishat <qtaisha.ao@gmail.com>
2023-01-30 09:13:05 +0100

    a class Square that defines a square by: Private instance attribute: size:Public instance method: def my_print(self): that prints in stdout the square with the character #:

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""this module defines a class Square and defines a public instsnce
+method that prints in stdout the square with the character #"""
+
+
+class Square:
+    """class created"""
+
+    def __init__(self, size=0):
+        """this method instantiates the class"""
+
+        self.size = size
+
+    @property
+    def size(self):
+        """this method retrieves the value of attribute size"""
+
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """this method sets the value of attribute size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """this method returns the current square area"""
+
+        square_area = self.__size ** 2
+        return(square_area)
+
+    def my_print(self):
+        """this method prints in stdout the square with the character #"""
+
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#", end='')
