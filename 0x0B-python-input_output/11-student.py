dd5e37b8b9687c43bfd938bdd26d995748b24bbe
Alausa Aishat <qtaisha.ao@gmail.com>
2023-02-13 15:14:37 +0100

    a class Student that defines a student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""this module contains a class Student that defines a student
+by public instances first_name, last_name, age"""
+
+
+class Student:
+    """class created"""
+
+    def __init__(self, first_name, last_name, age):
+        """this method is called when class is instantiated and it
+        initializes with first_name, last_name, age"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """this method retrieves a dictionary representation of a
+        Student instance"""
+
+        new_dict = {}
+
+        if type(attrs) is list:
+            for i in attrs:
+                for key, value in self.__dict__.items():
+                    if i == key:
+                        new_dict[key] = value
+            return(new_dict)
+        return(self.__dict__)
+
+    def reload_from_json(self, json):
+        """this method replaces all attributes of the Student instance"""
+
+        for key, value in json.items():
+            setattr(self, key, value)
