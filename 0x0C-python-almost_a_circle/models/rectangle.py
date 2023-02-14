80f53be260fa475dc98cc375b4d48d806358ad0a
Alausa Aishat <qtaisha.ao@gmail.com>
2023-02-14 01:01:56 +0100

    class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,72 +11,72 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """method initializes instance with stated attributes"""
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        @property
-        def width(self):
-            """this method retrieves the value of the attribute width"""
+    @property
+    def width(self):
+        """this method retrieves the value of the attribute width"""
 
-            return(self.__width)
+        return(self.__width)
 
-        @width.setter
-        def width(self, value):
-            """this method sets the value of the attribute width"""
+    @width.setter
+    def width(self, value):
+        """this method sets the value of the attribute width"""
 
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """this method retrieves the value of the attribute height"""
+    @property
+    def height(self):
+        """this method retrieves the value of the attribute height"""
 
-            return(self.__height)
+        return(self.__height)
 
-        @height.setter
-        def height(self, value):
-            """this method sets the value of the attribute height"""
+    @height.setter
+    def height(self, value):
+        """this method sets the value of the attribute height"""
 
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """this method retrieves the value of the attribute x"""
+    @property
+    def x(self):
+        """this method retrieves the value of the attribute x"""
 
-            return(self.__x)
+        return(self.__x)
 
-        @x.setter
-        def x(self, value):
-            """this method sets the value of the attribute x"""
+    @x.setter
+    def x(self, value):
+        """this method sets the value of the attribute x"""
 
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """this method retrieves the value of the attribute x"""
+    @property
+    def y(self):
+        """this method retrieves the value of the attribute x"""
 
-            return(self.__y)
+        return(self.__y)
 
-        @y.setter
-        def y(self, value):
-            """this method sets the value of the attribute y"""
+    @y.setter
+    def y(self, value):
+        """this method sets the value of the attribute y"""
 
-            if type(value) is not int:
-                raise TypeError("y must be an integer")
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
