53bbbabb98477d7461297a16829875f428c1267e
Alausa Aishat <qtaisha.ao@gmail.com>
2023-02-17 08:40:18 +0100

    class square updated with the public getter and setter methods for size attribute

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,6 +14,23 @@ class Square(Rectangle):
         self.size = size
         super().__init__(self.size, self.size, x, y, id)
 
+    @property
+    def size(self):
+        """public getter mathod for size attribute"""
+
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """the setter method for size attribute"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
     def __str__(self):
         """this method overrides the inbuilt __str__ method"""
         return("[Square] ({}) {}/{} - {}".format(self.id,
