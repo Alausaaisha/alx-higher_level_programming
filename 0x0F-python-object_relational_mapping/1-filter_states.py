96bbdebf4a6f726ac42a0fac468f8465b14541a7
Alausa Aishat <qtaisha.ao@gmail.com>
2023-04-15 23:30:49 +0100

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module lists all states with a name tarting with N from
+the database hbtn_0e_0_usa
+script should take 3 arguments: mysql username, mysql password
+and database name
+script should connect to a MySQL server running on localhost at
+port 3306
+Results must be sorted in ascending order by states.id"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    db.close()
