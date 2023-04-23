0b78492c6231f489883cad95ad301dec95496cb5
Alausa Aishat <qtaisha.ao@gmail.com>
2023-04-23 07:59:19 +0100

    new state object to database

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""this module adds the State object “Louisiana” to the database
+hbtn_0e_6_usa
+script should take 3 arguments: mysql username, mysql password
+and database name
+must use the module SQLAlchemy
+must import State and Base from model_state - from model_state
+import Base, State
+script should connect to a MySQL server running on localhost at port 3306
+Print the new states.id after creation
+code should not be executed when imported"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+# create connection
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(argv[1], argv[2], argv[3]))
+
+# create all tables in the metadata
+Base.metadata.create_all(engine)
+
+# create session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# add new state object
+new_obj = State(name='Louisiana')
+
+session.add(new_obj)
+session.commit()
+
+# query
+state = session.query(State).filter_by(name='Louisiana').first()
+if state is not None:
+    print('{}'.format(state.id))
+else:
+    print('Not Found')
+session.close()
