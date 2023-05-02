61223c9035a72dfd35579033c7c1d5e39d8cf045
Alausa Aishat <qtaisha.ao@gmail.com>
2023-05-02 09:59:04 +0100

    this script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response. Your script must send a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request. You have to use curl
-curl -d @$2 $1
+curl -s -X POST -H "Content-Type: application/json" -d @$2 $1
