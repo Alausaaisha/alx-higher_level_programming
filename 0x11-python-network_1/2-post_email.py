00d9ba1a7651fbc2f5188e1a046dbf3e4fc0f80d
Alausa Aishat <qtaisha.ao@gmail.com>
2023-05-03 08:47:30 +0100

    and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     post_dict = {'email': argv[2]}
-    url_encoded_data = urlencode(post_data)
+    url_encoded_data = urlencode(post_dict)
     post_data = url_encoded_data.encode("utf-8")
     body, response = make_request(argv[1], data=post_data)
     print(body.decode("utf-8"))
