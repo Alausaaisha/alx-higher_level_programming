596e31efb1f6d986f142011d078d8e84feda5f1d
Alausa Aishat <qtaisha.ao@gmail.com>
2023-05-19 16:00:57 +0100

    Python script that takes 2 arguments in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,7 +13,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'\
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
     commits = r.json()
     for commit in commits[:10]:
